a21e82913f3dc7bea1053cfa10cfa9ce31b3df3c
Huw Cheston <hwc31@cam.ac.uk>
2022-01-25 15:07:18 +0000

    Finished refactoring CamThread.py

--- CamThread.py
+++ CamThread.py
@@ -19,40 +19,43 @@ from collections import deque
 class CamThread:
     def __init__(self, source: int, stop_event: threading.Event, global_barrier: threading.Barrier, params: dict):
 
-        # Define threads and thread arguments
-        methods = [self.camera_reader,]
-        target_args = (stop_event, global_barrier)
-        threads = [threading.Thread(target=method, args=target_args) for method in methods]
-
-        # Define class attributes
-        queue_length = 128  # This can be changed to save memory if required
-        self.researcher_cam_queue = Queue(maxsize=queue_length)
-        self.performer_cam_queue = Queue(maxsize=queue_length)
-        self.local_barrier = threading.Barrier(len(threads))   # Barrier parties should equal number of running threads
-        # FIXME: Local barrier won't have the correct number of threads now I've refactored!
-
         self.source = source
         self.params = params
+        self.queue_length = 128  # This can be changed to save memory if required
+        self.researcher_cam_queue = Queue(maxsize=self.queue_length)
+        self.performer_cam_queue = Queue(maxsize=self.queue_length)
 
+        # Define threads and thread arguments
+        classes = [CamRead(source=self.source, performer_cam_queue=self.performer_cam_queue, researcher_cam_queue=self.researcher_cam_queue),
+                   ResearcherCamView(source=self.source, queue=self.researcher_cam_queue),
+                   PerformerCamView(source=self.source, queue=self.performer_cam_queue, params=self.params),
+                   CamWrite(source=self.source)]
+        # FIXME: local barrier needs fixing (is it necessary?)
+        local_barrier = threading.Barrier(1)
+        args = (local_barrier, global_barrier, stop_event)
+        self.threads = [threading.Thread(target=cl.start_cam, args=args) for cl in classes]
+        self.start_threads()
 
-        res_view = ResearcherCamView(source=self.source, queue=self.researcher_cam_queue)
-        threads.append(threading.Thread(target=res_view.start_cam, args=(self.local_barrier, global_barrier, stop_event)))
-        per_view = PerformerCamView(source=self.source, queue=self.performer_cam_queue, params=self.params)
-        threads.append(threading.Thread(target=per_view.start_cam, args=(self.local_barrier, global_barrier, stop_event)))
-        writer = CamWrite(source=self.source)
-        threads.append(threading.Thread(target=writer.start_cam, args=(global_barrier, self.local_barrier, stop_event)))
-
-        # Start threads
-        for thread in threads:
+    def start_threads(self):
+        for thread in self.threads:
             thread.start()
 
-    def camera_reader(self, stop_event, global_barrier):
-        """
-        Grabs frame from USB camera source, puts to researcher/performer cam queue.
-        """
-        # Initialisation
-        cam = cv2.VideoCapture(self.source, cv2.CAP_DSHOW)
-        _, frame = cam.read()
+
+class CamRead:
+    def __init__(self, source, researcher_cam_queue, performer_cam_queue):
+        self.source = source
+        self.cam = cv2.VideoCapture(self.source, cv2.CAP_DSHOW)
+        self.researcher_cam_queue = researcher_cam_queue
+        self.performer_cam_queue = performer_cam_queue
+
+    def start_cam(self, local_barrier, global_barrier, stop_event):
+        self.read_frame()
+        self.wait(local_barrier, global_barrier)
+        self.main_loop(stop_event)
+        self.exit_loop()
+
+    def read_frame(self):
+        _, frame = self.cam.read()
         if frame is None:
             print('no frame!')
             os._exit(1)
@@ -60,60 +63,19 @@ class CamThread:
             self.researcher_cam_queue.put(frame)
             self.performer_cam_queue.put(frame)
 
-        # Wait for other threads to initialise
+    def wait(self, global_barrier, local_barrier):
         print(f"Cam {self.source + 1} reader currently waiting. Waiting threads = {global_barrier.n_waiting + 1}")
-        self.local_barrier.wait()
+        local_barrier.wait()
         global_barrier.wait()
 
-        # Main loop
+    def main_loop(self, stop_event):
         while not stop_event.is_set():
-            # Read frame from webcam
-            _, frame = cam.read()
-
-            # Put frame in queue for modification and viewing
+            _, frame = self.cam.read()
             self.researcher_cam_queue.put(frame)
             self.performer_cam_queue.put(frame)
 
-        # Exit loop, cleanup and close thread
-        cam.release()
-
-    def write_video(self, stop_event, global_barrier):
-        pass
-
-
-def initialise_camera(n: str, q: Queue) -> None:
-    while True:
-        try:
-            frame = q.get(False)
-        except Empty:
-            time.sleep(1)
-        else:
-            cv2.imshow(n, frame)
-            break
-    return
-
-
-def get_video_stats(filename):
-    # TODO: implement this function
-    vid = cv2.VideoCapture(filename)
-    fps = vid.get(cv2.CAP_PROP_FPS)
-    framecount = vid.get(cv2.CAP_PROP_FRAME_COUNT)
-    duration = framecount / fps
-    print(duration)
-
-
-class CamRead:
-    def __init__(self):
-        pass
-
-    def initialise(self):
-        pass
-
-    def wait(self):
-        pass
-
-    def main_loop(self):
-        pass
+    def exit_loop(self):
+        self.cam.release()
 
 
 class ResearcherCamView:
@@ -162,6 +124,7 @@ class PerformerCamView:
         global_barrier.wait()
 
     def main_loop(self, stop_event):
+        # TODO: fix this 'magic number'
         frames = deque(maxlen=150)
         while not stop_event.is_set():
             frame = self.queue.get()
@@ -214,3 +177,24 @@ class CamWrite:
     def exit_loop(self):
         # Print stats about the video file
         get_video_stats(self.filename)
+
+
+def initialise_camera(n: str, q: Queue) -> None:
+    while True:
+        try:
+            frame = q.get(False)
+        except Empty:
+            time.sleep(1)
+        else:
+            cv2.imshow(n, frame)
+            break
+    return
+
+
+def get_video_stats(filename):
+    # TODO: implement this function
+    vid = cv2.VideoCapture(filename)
+    fps = vid.get(cv2.CAP_PROP_FPS)
+    framecount = vid.get(cv2.CAP_PROP_FRAME_COUNT)
+    duration = framecount / fps
+    print(duration)
