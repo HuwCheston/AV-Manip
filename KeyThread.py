495dab72b3b17830b57e37404dcfa1f1a5c1b1f1
Huw Cheston <hwc31@cam.ac.uk>
2022-01-25 21:46:44 +0000

    Finished KeyThread.py refactor.

--- KeyThread.py
+++ KeyThread.py
@@ -2,31 +2,26 @@ from cv2 import cv2
 import threading
 from numpy import zeros, uint8
 
-# TODO: refactor in the same manner as CamThread
+
 class KeyThread:
     def __init__(self, params: dict, stop_event: threading.Event,):
         self.blank_image = zeros(shape=[100, 100, 3], dtype=uint8)  # Blank image for keypress manager to display
-
-        # Define threads
-        keypress_manager = threading.Thread(target=self.main_loop, args=(stop_event, params))
+        self.name = 'Keypress Manager'
+        keypress_manager = threading.Thread(target=self.start_keymanager, args=(stop_event, params))
         keypress_manager.start()
 
+    def start_keymanager(self, stop_event, params):
+        self.cv2_setup()
+        self.main_loop(stop_event, params)
+        self.exit_loop()
+
+    def cv2_setup(self):
+        cv2.namedWindow(self.name, cv2.WINDOW_NORMAL)
+        cv2.setWindowProperty(self.name, cv2.WND_PROP_TOPMOST, 1)  # Keep the keypress manager on top
+
     def main_loop(self, stop_event, params):
-        """
-        Creates a new CV2 window, listens for keypresses, sends these to KeyThread/CamThread to trigger manipulations.
-        """
-        # Initialisation
-        cv2.namedWindow('Keypress Manager', cv2.WINDOW_NORMAL)
-        cv2.setWindowProperty('Keypress Manager', cv2.WND_PROP_TOPMOST, 1)  # Keep the keypress manager on top
-
-        # Wait for other threads to initialise
-        # There is no need for the keypress manager to start at the same time as the video/audio recording - so there is
-        # no use of the global_barrier to block it while the other threads initialise, as with CamThread and ReaThread
-
-        # Main loop
         while not stop_event.is_set():
-            cv2.imshow('Keypress Manager', self.blank_image)
-
+            cv2.imshow(self.name, self.blank_image)
             # Detects keypresses to trigger modifications in CamThread and ReaThread
             key = chr(cv2.waitKey(1) % 255)
             match key:
@@ -45,5 +40,5 @@ class KeyThread:
                 case 'q':
                     stop_event.set()
 
-        # Exit loop, cleanup and close thread
-        cv2.destroyWindow('Keypress Manager')
+    def exit_loop(self):
+        cv2.destroyWindow(self.name)
