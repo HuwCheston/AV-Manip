822fec73db1dae8bd44dbd04fd15b19640bba75f
Huw Cheston <hwc31@cam.ac.uk>
2022-01-25 15:30:13 +0000

    Implemented pattern matching in CamThread.py + general tidying.

--- CamThread.py
+++ CamThread.py
@@ -6,13 +6,6 @@ import os
 from queue import Queue, Empty
 from datetime import datetime
 from collections import deque
-
-# If you run into FileNotFound errors when importing ffmpeg-python, make sure that ffmpeg.exe is placed in the
-# Python/Scripts directory of your environment.
-
-# TODO: Refactor this into a group of four classes, all created by the CamThread. Each class should have the same
-#  initialise/wait/loop functions, but split into different methods.
-
 # TODO: investigate using PyTest here!
 
 
@@ -21,18 +14,16 @@ class CamThread:
 
         self.source = source
         self.params = params
-        self.queue_length = 128  # This can be changed to save memory if required
-        self.researcher_cam_queue = Queue(maxsize=self.queue_length)
-        self.performer_cam_queue = Queue(maxsize=self.queue_length)
+        self.queue_length = 5  # This can be changed to save memory if required
+        researcher_cam_queue = Queue(maxsize=self.queue_length)
+        performer_cam_queue = Queue(maxsize=self.queue_length)
 
         # Define threads and thread arguments
-        classes = [CamRead(source=self.source, performer_cam_queue=self.performer_cam_queue, researcher_cam_queue=self.researcher_cam_queue),
-                   ResearcherCamView(source=self.source, queue=self.researcher_cam_queue),
-                   PerformerCamView(source=self.source, queue=self.performer_cam_queue, params=self.params),
+        classes = [CamRead(source=self.source, performer_q=performer_cam_queue, researcher_q=researcher_cam_queue),
+                   ResearcherCamView(source=self.source, queue=researcher_cam_queue),
+                   PerformerCamView(source=self.source, queue=performer_cam_queue, params=self.params),
                    CamWrite(source=self.source)]
-        # FIXME: local barrier needs fixing (is it necessary?)
-        local_barrier = threading.Barrier(1)
-        args = (local_barrier, global_barrier, stop_event)
+        args = (global_barrier, stop_event)
         self.threads = [threading.Thread(target=cl.start_cam, args=args) for cl in classes]
         self.start_threads()
 
@@ -42,15 +33,15 @@ class CamThread:
 
 
 class CamRead:
-    def __init__(self, source, researcher_cam_queue, performer_cam_queue):
+    def __init__(self, source, researcher_q, performer_q):
         self.source = source
         self.cam = cv2.VideoCapture(self.source, cv2.CAP_DSHOW)
-        self.researcher_cam_queue = researcher_cam_queue
-        self.performer_cam_queue = performer_cam_queue
+        self.researcher_cam_queue = researcher_q
+        self.performer_cam_queue = performer_q
 
-    def start_cam(self, local_barrier, global_barrier, stop_event):
+    def start_cam(self, global_barrier, stop_event):
         self.read_frame()
-        self.wait(local_barrier, global_barrier)
+        self.wait(global_barrier)
         self.main_loop(stop_event)
         self.exit_loop()
 
@@ -63,9 +54,8 @@ class CamRead:
             self.researcher_cam_queue.put(frame)
             self.performer_cam_queue.put(frame)
 
-    def wait(self, global_barrier, local_barrier):
+    def wait(self, global_barrier):
         print(f"Cam {self.source + 1} reader currently waiting. Waiting threads = {global_barrier.n_waiting + 1}")
-        local_barrier.wait()
         global_barrier.wait()
 
     def main_loop(self, stop_event):
@@ -83,14 +73,13 @@ class ResearcherCamView:
         self.name = f"Cam {source + 1} Rec"
         self.queue = queue
 
-    def start_cam(self, local_barrier, global_barrier, stop_event):
+    def start_cam(self, global_barrier, stop_event):
         initialise_camera(n=self.name, q=self.queue)
-        self.wait(local_barrier, global_barrier)
+        self.wait(global_barrier)
         self.main_loop(stop_event)
         self.exit_loop()
 
-    def wait(self, local_barrier, global_barrier):
-        local_barrier.wait()
+    def wait(self, global_barrier):
         print(f"{self.name} currently waiting. Waiting threads = {global_barrier.n_waiting + 1}\n")
         global_barrier.wait()
 
@@ -112,31 +101,30 @@ class PerformerCamView:
         self.queue = queue
         self.params = params
 
-    def start_cam(self, local_barrier, global_barrier, stop_event):
+    def start_cam(self, global_barrier, stop_event):
         initialise_camera(n=self.name, q=self.queue)
-        self.wait(local_barrier, global_barrier)
+        self.wait(global_barrier)
         self.main_loop(stop_event)
         self.exit_loop()
 
-    def wait(self, local_barrier, global_barrier):
-        local_barrier.wait()
+    def wait(self, global_barrier):
         print(f"{self.name} currently waiting. Waiting threads = {global_barrier.n_waiting + 1}\n")
         global_barrier.wait()
 
     def main_loop(self, stop_event):
-        # TODO: fix this 'magic number'
-        frames = deque(maxlen=150)
+        fps = 30
+        lag_secs = 5    # This can be edited to set a different timeframe to lag the performer video by
+        frames = deque(maxlen=(fps*lag_secs))
         while not stop_event.is_set():
             frame = self.queue.get()
             frames.append(frame)  # Frames are added to the deque so they can be played later (for delay effect)
 
-            # TODO: replace this with match - case syntax (see KeyThread)
             # Modify frame
-            if self.params['flipped']:
-                frame = cv2.flip(frame, 0)
-            if self.params['delayed']:
-                frame = frames[1]
-
+            match self.params:
+                case {'flipped': True}:
+                    frame = cv2.flip(frame, 0)
+                case {'delayed': True}:
+                    frame = frames[1]
             cv2.imshow(self.name, frame)
             cv2.waitKey(1)
 
@@ -146,15 +134,17 @@ class PerformerCamView:
 
 
 class CamWrite:
+    # If you run into FileNotFound errors when importing ffmpeg-python, make sure that ffmpeg.exe is placed in the
+    # Python/Scripts directory of your environment.
     def __init__(self, source: int):
         self.source = source
         self.window_name = f"Cam {self.source + 1} Rec"
         self.filename = f'output/video/{datetime.now().strftime("%d-%m-%y_%H.%M.%S")}_cam{self.source + 1}_out.avi'
-        # TODO: fix ffmpeg flags to ensure highest quality of output
 
-    def start_cam(self, local_barrier, global_barrier, stop_event):
+    def start_cam(self, global_barrier, stop_event):
         print('cam write running on latest version')
-        self.wait(local_barrier, global_barrier)
+        self.wait(global_barrier)
+        # TODO: fix ffmpeg flags to ensure highest quality of output
         process = (
             ffmpeg.input(format='gdigrab', framerate="30", filename=f"title={self.window_name}", loglevel='warning',
                          probesize='500M')
@@ -162,8 +152,7 @@ class CamWrite:
         self.main_loop(stop_event, process)
         self.exit_loop()
 
-    def wait(self, local_barrier, global_barrier):
-        local_barrier.wait()
+    def wait(self, global_barrier):
         print(f"Cam {self.source + 1} Writer currently waiting. Waiting threads = {global_barrier.n_waiting + 1}\n")
         global_barrier.wait()
 
@@ -192,7 +181,7 @@ def initialise_camera(n: str, q: Queue) -> None:
 
 
 def get_video_stats(filename):
-    # TODO: implement this function
+    # TODO: implement this function (as a separate file?)
     vid = cv2.VideoCapture(filename)
     fps = vid.get(cv2.CAP_PROP_FPS)
     framecount = vid.get(cv2.CAP_PROP_FRAME_COUNT)
