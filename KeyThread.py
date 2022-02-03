6a6e2b84a32c420f0333d49e6f67dc876af5d127
Huw Cheston <hwc31@cam.ac.uk>
2022-02-03 14:17:18 +0000

    Improved GUI with .grid()

--- KeyThread.py
+++ KeyThread.py
@@ -8,11 +8,13 @@ class KeyThread:
         self.stop_event = stop_event
         self.root = tk.Tk()
         self.root.title = self.name
-        self.start_keymanager(global_barrier, params)
+        self.params = params
+        self.tk_list = []
+        self.start_keymanager(global_barrier)
 
-    def start_keymanager(self, global_barrier, params):
+    def start_keymanager(self, global_barrier):
         self.wait(global_barrier)
-        self.tk_setup(params)
+        self.tk_setup()
         self.main_loop()
 
     def wait(self, global_barrier):
@@ -20,66 +22,87 @@ class KeyThread:
         global_barrier.wait()
 
     # TODO: refactor these into multiple functions (or class?)
-    def tk_setup(self, params):
-        def set_delay():
-            try:
-                d = int(d_time.get())
-            except ValueError:
-                d_time.delete(0, 'end')
-                d_time.insert(0, 'Not a number')
-            else:
-                if 0 < d < params['*max delay time']:
-                    params['*delay time'] = d
-                else:
-                    d_time.delete(0, 'end')
-                    d_time.insert(0, 'Out of bounds')
+    def tk_setup(self):
+        command_tk_list = [tk.Label(text='Commands'),
+                           tk.Button(self.root, text="Reset", command=self._reset_manips),
+                           tk.Button(self.root, text="Quit", command=self.exit_loop)]
+        self._organise_gui(tk_list=command_tk_list, col_num=2)
 
-        def manipulate(manip, button):
-            reset()
-            params[manip] = True
-            button.config(bg='green')
+        delay_tk_list = []
+        self._populate_tk_list(tk_list=delay_tk_list, manip_str='delay')
+        d_time = tk.Entry(self.root)
+        d_time.insert(0, str(self.params['*delay time']))
+        delay_tk_list.append(d_time)
+        delay_tk_list.append(
+            tk.Button(self.root, text='Set Delay Time', command=lambda: self._set_delay_manip_time(d_time)))
+        for (k, v) in self.params["*delay time presets"].items():
+            delay_tk_list.append(tk.Button(self.root, text=f'{k} - {str(v)}',
+                                           command=lambda x=v: [d_time.delete(0, 'end'),
+                                                                d_time.insert(0, str(x)),
+                                                                self._set_delay_manip_time(d_time)]))
+        self._organise_gui(tk_list=delay_tk_list, col_num=3)
 
-        def reset():
-            # TODO: can this be replaced with an event? Will need to change ReaThread too
-            params['*reset'] = True
-            for button in tk_list:
-                if isinstance(button, tk.Button):
-                    button.config(bg="SystemButtonFace")
-            params['*reset lock'].acquire()
-            for param in params.keys():
-                if isinstance(params[param], bool):
-                    params[param] = False
+        loop_tk_list = []
+        self._populate_tk_list(tk_list=loop_tk_list, manip_str='loop')
+        self._organise_gui(tk_list=loop_tk_list, col_num=4)
 
-        tk_list = []
-        canvas = tk.Canvas(self.root, width=500, height=400, bd=0, highlightthickness=0)
-        tk_list.append(canvas)
+        blank_tk_list = []
+        self._populate_tk_list(tk_list=blank_tk_list, manip_str='blank')
+        self._organise_gui(tk_list=blank_tk_list, col_num=5)
 
-        d_time = tk.Entry(canvas)
-        d_time.insert(0, str(params['*delay time']))
-        tk_list.append(d_time)
-        tk_list.append(tk.Button(canvas, text='Set Delay Time', command=set_delay))
+        control_tk_list = []
+        self._populate_tk_list(tk_list=control_tk_list, manip_str='control')
+        self._organise_gui(tk_list=control_tk_list, col_num=6)
 
-        for p in params.keys():
-            if not p.startswith('*'):
-                b = tk.Button(canvas, text=p.title())
-                b.config(fg='black', command=lambda manip=p, button=b: manipulate(manip, button))
+        extra_manip_tk_list = []
+
+        biglist = [command_tk_list, delay_tk_list, loop_tk_list, blank_tk_list, control_tk_list, extra_manip_tk_list]
+        self.tk_list = [item for sublist in biglist for item in sublist]
+
+    def _populate_tk_list(self, tk_list, manip_str):
+        tk_list.append(tk.Label(text=manip_str.title()))
+        for k in self.params.keys():
+            if k.startswith(manip_str):
+                b = tk.Button(self.root, text=k.title())
+                b.config(fg='black', command=lambda manip=k, button=b: self._set_manip(manip, button))
                 tk_list.append(b)
-            elif p == '*reset':
-                tk_list.append(tk.Button(canvas, text=p[1:].title(), command=reset))
-            elif p == '*quit':
-                tk_list.append(tk.Button(canvas, text=p[1:].title(), command=self.exit_loop))
-            elif p == '*delay time presets':
-                for (k, v) in params[p].items():
-                    tk_list.append(
-                        tk.Button(canvas, text=f'{k} - {str(v)}', command=lambda x=v: [d_time.delete(0, 'end'),
-                                                                                       d_time.insert(0, str(x)),
-                                                                                       set_delay()]))
 
+    def _set_delay_manip_time(self, d_time):
+        try:
+            d = int(d_time.get())
+        except ValueError:
+            d_time.delete(0, 'end')
+            d_time.insert(0, 'Not a number')
+        else:
+            if 0 < d < self.params['*max delay time']:
+                self.params['*delay time'] = d
+            else:
+                d_time.delete(0, 'end')
+                d_time.insert(0, 'Out of bounds')
+
+    def _set_manip(self, manip, button):
+        self._reset_manips()
+        self.params[manip] = True
+        button.config(bg='green')
+
+    def _reset_manips(self):
+        # TODO: can this be replaced with an event? Will need to change ReaThread too
+        self.params['*reset'] = True
+        for button in self.tk_list:
+            if isinstance(button, tk.Button):
+                button.config(bg="SystemButtonFace")
+        self.params['*reset lock'].acquire()
+        for param in self.params.keys():
+            if isinstance(self.params[param], bool):
+                self.params[param] = False
+
+    def _organise_gui(self, tk_list, col_num):
         # TODO: Surely there's a better way to organise the GUI than this...
-        for b in tk_list:
-            b.pack()
+        for row_num, b in enumerate(tk_list):
+            b.grid(row=row_num, column=col_num, padx=10, pady=1)
 
     def main_loop(self):
+        self.root.attributes('-topmost', 'true')
         self.root.mainloop()
 
     def exit_loop(self):
