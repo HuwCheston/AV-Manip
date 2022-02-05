eec7320240d40149bc1f041011e67efd089bb82e
Huw Cheston <hwc31@cam.ac.uk>
2022-02-05 14:31:24 +0000

    Major GUI refactor -> seperate funcs

--- KeyThread.py
+++ KeyThread.py
@@ -21,75 +21,108 @@ class KeyThread:
         global_barrier.wait()
 
     def tk_setup(self):
-        self.root.wm_title("AV-Manip")
-        self.root.iconbitmap("cms-logo.ico")
+        self.info_pane(1)
+        self.command_pane(2)
+        self.delay_pane(3)
+        self.loop_pane(4)
+        self.blank_pane(5)
+        self.control_pane(6)
+        self.flip_pane(7)
 
-        # TODO: un-spaghetti this!
+    def main_loop(self):
+        self.root.attributes('-topmost', 'true')
+        self.root.mainloop()
+
+    def exit_loop(self):
+        self.stop_event.set()
+        self.root.destroy()
+
+    def info_pane(self, col_num):
         info_frame = tk.Frame(self.root, padx=10, pady=1)
-        info_frame.grid(row=1, column=1)
+        info_frame.grid(row=1, column=col_num)
         img_label = tk.Label(info_frame)
         img_label.image = tk.PhotoImage(file="cms-logo.gif")
         img_label['image'] = img_label.image
-        img_label.grid(row=1, column=1, sticky="n", padx=10, pady=10)
+        img_label.grid(row=1, column=col_num, sticky="n", padx=10, pady=10)
         version = tk.Label(info_frame, text='AV-Manip (v.0.1)')
-        version.grid(row=2, column=1)
+        version.grid(row=2, column=col_num)
         # TODO: make this a hyperlink (see stack overflow)
         copyright = tk.Label(info_frame, text='© Huw Cheston, 2022')
-        copyright.grid(row=3, column=1)
+        copyright.grid(row=3, column=col_num)
         participants = tk.Label(info_frame, text=f'Active participants: {str(self.params["*participants"])}')
-        participants.grid(row=4, column=1)
+        participants.grid(row=4, column=col_num)
         participants = tk.Label(info_frame, text=f'Camera FPS: {str(self.params["*fps"])}')
-        participants.grid(row=5, column=1)
+        participants.grid(row=5, column=col_num)
 
+    def command_pane(self, col_num):
         command_tk_frame = tk.Frame(self.root, borderwidth=2, relief="groove")
         command_tk_list = [tk.Label(command_tk_frame, text='Commands'),
-                           tk.Button(command_tk_frame, text="Reset", command=self._reset_manips),
+                           tk.Button(command_tk_frame, text="Reset", command=self.reset_manips),
                            tk.Button(command_tk_frame, text="Quit", command=self.exit_loop)]
-        self._organise_gui(tk_list=command_tk_list, col_num=2)
+        self.organise_gui(tk_list=command_tk_list, col_num=col_num)
         command_tk_frame.grid(column=2, row=1, sticky="n", padx=10, pady=10)
 
+    def delay_pane(self, col_num):
         delay_params_tk_frame = tk.Frame(self.root, borderwidth=2, relief="groove")
         delay_time_frame = tk.Frame(delay_params_tk_frame)
-        d_time = tk.Entry(delay_time_frame, width=5)
+        d_time = tk.Entry(delay_time_frame, width=15)
         d_time.insert(0, str(self.params['*delay time']))
         msec = tk.Label(delay_time_frame, text='msec')
-        delay_params_tk_list = [tk.Label(delay_params_tk_frame, text='Delay')]
+        delay_tk_list = [tk.Label(delay_params_tk_frame, text='Delay')]
         b = tk.Button(delay_params_tk_frame, text='Start Delay')
-        b.config(fg='black', command=lambda manip='delayed', button=b: self._set_manip(manip, button))
-        delay_params_tk_list.append(b)
-        delay_params_tk_list.append(tk.Button(delay_params_tk_frame, text='Set Delay Time', command=lambda: self._set_delay_manip_time(d_time)))
+        b.config(fg='black', command=lambda manip='delayed', button=b: self.enable_manip(manip, button))
+        delay_tk_list.append(b)
+        delay_tk_list.append(tk.Button(delay_params_tk_frame, text='Set Delay Time', command=lambda: self.set_delay_time(d_time)))
         for (k, v) in self.params["*delay time presets"].items():
-            delay_params_tk_list.append(tk.Button(delay_params_tk_frame, text=f'{k} - {str(v)} msec',
+            delay_tk_list.append(tk.Button(delay_params_tk_frame, text=f'{k} - {str(v)} msec',
                                            command=lambda x=v: [d_time.delete(0, 'end'),
                                                                 d_time.insert(0, str(x)),
-                                                                self._set_delay_manip_time(d_time)]))
-        self._organise_gui(tk_list=delay_params_tk_list, col_num=1)
-        delay_params_tk_frame.grid(column=3, row=1, sticky="n", padx=10, pady=10)
+                                                                self.set_delay_time(d_time)]))
+        self.organise_gui(tk_list=delay_tk_list, col_num=1)
+        delay_params_tk_frame.grid(column=col_num, row=1, sticky="n", padx=10, pady=10)
         delay_time_frame.grid(column=1, row=10, sticky="n", padx=10, pady=10)
         d_time.grid(row=1, column=1, sticky='n')
         msec.grid(row=1, column=2, sticky='n')
+        self.tk_list.extend(delay_tk_list)
 
-        manip_str = ['loop', 'blank', 'control', 'flip']
-        self.tk_list = [item for sublist in [self._populate_tk_list(manip_str=manip_str[num], col_num=num+4)
-                                             for num in range(len(manip_str))] for item in sublist]
+    def loop_pane(self, col_num):
+        manip_str = 'loop'
+        self.populate_tk_list(manip_str, col_num=col_num)
 
-    def _populate_tk_list(self, manip_str, col_num):
+    def blank_pane(self, col_num):
+        manip_str = 'blank'
+        self.populate_tk_list(manip_str, col_num=col_num)
+
+    def control_pane(self, col_num):
+        manip_str = 'control'
+        self.populate_tk_list(manip_str, col_num=col_num)
+
+    def flip_pane(self, col_num):
+        manip_str = 'flip'
+        self.populate_tk_list(manip_str, col_num=col_num)
+
+    def populate_tk_list(self, manip_str, col_num):
         frame = tk.Frame(self.root, borderwidth=2, relief="groove")
         tk_list = [tk.Label(frame, text=manip_str.title())]
         for k in self.params.keys():
             if k.startswith(manip_str):
                 b = tk.Button(frame, text=k.title())
-                b.config(fg='black', command=lambda manip=k, button=b: self._set_manip(manip, button))
+                b.config(fg='black', command=lambda manip=k, button=b: self.enable_manip(manip, button))
                 tk_list.append(b)
-        self._organise_gui(tk_list=tk_list, col_num=col_num)
+        self.organise_gui(tk_list=tk_list, col_num=col_num)
         frame.grid(column=col_num, row=1, sticky="n", padx=10, pady=10)
-        return tk_list
+        self.tk_list.extend(tk_list)
 
-    def _organise_gui(self, tk_list, col_num):
+    def organise_gui(self, tk_list, col_num):
         for row_num, b in enumerate(tk_list):
             b.grid(row=row_num, column=col_num, padx=10, pady=1)
 
-    def _set_delay_manip_time(self, d_time):
+    def enable_manip(self, manip, button):
+        self.reset_manips()
+        self.params[manip] = True
+        button.config(bg='green')
+
+    def set_delay_time(self, d_time):
         try:
             d = int(d_time.get())
         except ValueError:
@@ -102,12 +135,7 @@ class KeyThread:
                 d_time.delete(0, 'end')
                 d_time.insert(0, 'Out of bounds')
 
-    def _set_manip(self, manip, button):
-        self._reset_manips()
-        self.params[manip] = True
-        button.config(bg='green')
-
-    def _reset_manips(self):
+    def reset_manips(self):
         # TODO: can this be replaced with an event? Will need to change ReaThread too
         self.params['*reset'] = True
         for button in self.tk_list:
@@ -117,11 +145,3 @@ class KeyThread:
         for param in self.params.keys():
             if isinstance(self.params[param], bool):
                 self.params[param] = False
-
-    def main_loop(self):
-        self.root.attributes('-topmost', 'true')
-        self.root.mainloop()
-
-    def exit_loop(self):
-        self.stop_event.set()
-        self.root.destroy()
