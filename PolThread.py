c951093dbfd10ef83dcb9aff88bcc16fc6a4775c
Huw Cheston <hwc31@cam.ac.uk>
2022-07-04 17:18:06 +0100

    Rewrote PolThread.py to add accelerometer tracking

--- PolThread.py
+++ PolThread.py
@@ -38,7 +38,7 @@ START_PPI = bytearray(      # Open PPI stream
     [0x02, 0x03]
 )
 START_ACC = bytearray(      # Open ACC stream
-    [0x02, 0x02, 0x00, 0x01, 0x34, 0x00, 0x01, 0x01, 0x10, 0x00, 0x02, 0x01, 0x08, 0x00, 0x04, 0x01, 0x03]
+    [0x02, 0x02, 0x00, 0x01, 0xd0, 0x00, 0x01, 0x01, 0x10, 0x00, 0x02, 0x01, 0x10, 0x00, 0x04, 0x01, 0x03]
 )
 
 """Timestamp constants"""
@@ -59,7 +59,7 @@ class PolThread:
         self.params = params
         self.logger = logger
 
-        # Results are appended into the corresponding lists here
+        # Results and raw data are appended into the corresponding lists here
         self.results = {s: [] for s in address[2]}
         self.raw_data = {s: [] for s in address[2]}
         # Used to log if its the first time a stream has reported data
@@ -88,27 +88,38 @@ class PolThread:
 
     async def _open_streams(self):
         """Gathers functions to open Polar streams. Doesn't run concurrently, as need to wait when SDK activated"""
+        async def enable_sdk():
+            nonlocal sdk_enabled
+            await self.client.write_gatt_char(UUID['PMD_Control'], SDK_MODE, response=True)
+            await asyncio.sleep(5)
+            sdk_enabled = True
+        sdk_enabled = False
         # Report initialisation
         await self.client.start_notify(UUID['PMD_Control'], self._report_init)
         # Gathering HR
         if 'hr' in self.results:
             # Receive HR data
             await self.client.start_notify(UUID['HR_UUID'], self._format_hr)
+        # Gathering ACC
+        if 'acc' in self.results:
+            # Start SDK mode if not on already
+            if not sdk_enabled:
+                await enable_sdk()
+            # Open acc stream
+            await self.client.write_gatt_char(UUID['PMD_Control'], START_ACC, response=True)
         # Gathering PPG
         if 'ppg' in self.results:
-            # Start SDK mode
-            await self.client.write_gatt_char(UUID['PMD_Control'], SDK_MODE, response=True)
-            await asyncio.sleep(5)
+            # Start SDK mode if not on already
+            if not sdk_enabled:
+                await enable_sdk()
             # Open PPG stream
             await self.client.write_gatt_char(UUID['PMD_Control'], START_PPG, response=True)
-            # Gather data from PPI stream
-            await self.client.start_notify(UUID['PMD_Data'], self._format_ppg)
         # Gathering PPI
         if 'ppi' in self.results:
             # Open PPI stream
             await self.client.write_gatt_char(UUID['PMD_Control'], START_PPI, response=True)
-            # Gather data from PPI stream
-            await self.client.start_notify(UUID['PMD_Data'], self._format_ppi)
+        # Gather data from open streams
+        await self.client.start_notify(UUID['PMD_Data'], self._format_pmd)
 
     def _close_streams(self):
         """Gathers functions to close open Polar streams"""
@@ -131,45 +142,19 @@ class PolThread:
         if data == bytearray([0xF0, 0x02, 0x09, 0x00, 0x00]):
             self.logger(f'{self.desc}: SDK mode enabled')
 
-    def _format_ppi(self, _, data):
-        """Formats incoming PPI stream, combines with OS timestamp and appends to required list"""
-        # Append raw data plus timestamp
-        if self.params['*recording']:
-            self.raw_data['ppi'].append((datetime.now(tz=timezone.utc).strftime(TIME_FMT), data))
-        type1, _, type2 = struct.unpack("<BqB", data[0:10])
-        # If data is from PPI stream
-        if type1 == 3:
-            num = math.floor((len(data) - 8) / 6)
-            for x in range(num):
-                start = 10 + x * 6
-                sample = data[start: start + 6]
-                hr, ppi, err, flags = struct.unpack("<BHHB", sample)
-                sample = {
-                    'address': self.address,
-                    'desc': self.desc,
-                    'timestamp': datetime.now(tz=timezone.utc).strftime(TIME_FMT),
-                    'heart_rate': hr,
-                    'ppi_ms': ppi,
-                    'error_estimate': err,
-                    'flags': flags
-                }
-                self._append_results(stream='ppi', data=sample)
+    def _format_pmd(self, _, data):
+        """Parses incoming data from PMD_Data, formats depending on stream"""
+        if data[0] == 0x01:
+            self._format_ppg(data=data)
+        elif data[0] == 0x02:
+            self._format_acc(data=data)
+        elif data[0] == 0x03:
+            self._format_ppi(data=data)
 
-    def _convert_to_timestamp(self, data, start, end):
-        """Converts bytearray to timestamp"""
-        return (
-            datetime.fromtimestamp(
-                (int.from_bytes(bytearray(data[start:end]), byteorder="little", signed=False,) / DIV) + OFFSET,
-                timezone.utc,
-            )
-        )
-
-    def _format_ppg(self, _, data):
+    def _format_ppg(self, data):
         def get_ppg_value(subdata):
             return struct.unpack("<i", subdata + (b'\0' if subdata[2] < 128 else b'\xff'))[0]
-        # Append raw data plus timestamp
-        if self.params['*recording']:
-            self.raw_data['ppg'].append((datetime.now(tz=timezone.utc).strftime(TIME_FMT), data))
+        self._append_raw(stream='ppg', raw=data)
         # Calculate number of delta frames
         num = math.floor((len(data) - 10) / 12)
         # Iterate through the delta frames
@@ -186,17 +171,60 @@ class PolThread:
             # Append the delta frame
             self._append_results(stream='ppg', data=sample)
 
+    def _format_acc(self, data):
+        """Formats incoming ACC stream - just appends to raw data list at the moment"""
+        self._append_raw(stream='acc', raw=data)
+        sample = {
+            'address': self.address,
+            'desc': self.desc,
+            'timestamp': datetime.now(tz=timezone.utc).strftime(TIME_FMT),
+            'data': data
+        }
+        self._append_results(stream='acc', data=sample,)
+
+    def _format_ppi(self, data):
+        """Formats incoming PPI stream, combines with OS timestamp and appends to required list"""
+        self._append_raw(stream='ppi', raw=data)
+        num = math.floor((len(data) - 8) / 6)
+        for x in range(num):
+            start = 10 + x * 6
+            sample = data[start: start + 6]
+            hr, ppi, err, flags = struct.unpack("<BHHB", sample)
+            sample = {
+                'address': self.address,
+                'desc': self.desc,
+                'timestamp': datetime.now(tz=timezone.utc).strftime(TIME_FMT),
+                'heart_rate': hr,
+                'ppi_ms': ppi,
+                'error_estimate': err,
+                'flags': flags
+            }
+            self._append_results(stream='ppi', data=sample,)
+
+    def _convert_array_to_signed_int(self, data, offset, length):
+        """Converts bytearray to signed integer"""
+        return int.from_bytes(
+            bytearray(data[offset: offset + length]), byteorder="little", signed=True,
+        )
+
+    def _convert_to_timestamp(self, data, start, end):
+        """Converts bytearray to timestamp"""
+        return (
+            datetime.fromtimestamp(
+                (int.from_bytes(bytearray(data[start:end]), byteorder="little", signed=False,) / DIV) + OFFSET,
+                timezone.utc,
+            )
+        )
+
     def _format_hr(self, _, data):
         """Appends reported heart rate and current OS time to HR list"""
-        # Append raw data plus timestamp
-        if self.params['*recording']:
-            self.raw_data['hr'].append((datetime.now(tz=timezone.utc).strftime(TIME_FMT), data))
         sample = {
             'address': self.address,
             'desc': self.desc,
             'timestamp': datetime.now(tz=timezone.utc).strftime(TIME_FMT),
             'heart_rate': data[1],
         }
+        self._append_raw(stream='hr', raw=data)
         self._append_results(stream='hr', data=sample)
 
     def start_polar(self, record_start):
@@ -210,13 +238,18 @@ class PolThread:
             report_data.append(self._save_data(data=v, ext=k))
         self.logger(self._report_results(streams=report_data))
 
-    def _append_results(self, stream: str, data: dict):
+    def _append_raw(self, stream: str, raw):
+        """If we're recording, append the raw data and system timestamp to required list"""
+        if self.params['*recording']:
+            self.raw_data[stream].append((datetime.now(tz=timezone.utc).strftime(TIME_FMT), raw))
+
+    def _append_results(self, stream: str, data: dict,):
         """Appends results to required list and logs when data is received for the first time"""
         # Check if this is the first time data has been received from a stream and log in GUI if so
         if self.is_firstrun[stream]:
             self.logger(f'{self.desc}: {stream.upper()} received')
             self.is_firstrun[stream] = False
-        # If we're recording, append the results to the required list
+        # If we're recording, append the results to required list
         if self.params['*recording']:
             self.results[stream].append(data)
 
@@ -248,9 +281,9 @@ class PolThread:
         results = f'{self.desc}: '
         for dat, cond in streams:
             if cond:
-                results += f'{dat.upper()} reported.'
+                results += f'{dat.upper()} reported.\n'
             else:
-                results += f'{dat.upper()} did not report.'
+                results += f'{dat.upper()} did not report.\n'
         return results
 
     def quit_polar(self):
